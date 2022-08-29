8b0b15c4a306cb1d655f6ab15eb7107ef96a4308
codinglil <102619991+codinglil@users.noreply.github.com>
2022-08-29 12:35:30 -0400

    follow video and deleting unnecessary info

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import "./Weather.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <h1>Weather Search Engine</h1>
-          <Weather defaultCity="Fairfax" />
-          <footer>
-            This project was coded by Lilia Gashin and is
-            <br />
-            <a
-              href="https://github.com/codinglil/weather-react-app1"
-              target="_blank"
-              rel="noreferrer"
-            >
-              open-sourced on GitHub
-            </a>
-          </footer>
-        </header>
-      </div>
+      <h1>Weather Search Engine</h1>
+      <Weather defaultCity="Fairfax" />
+      <footer>
+        This project was coded by Lilia Gashin and is
+        <br />
+        <a
+          href="https://github.com/codinglil/weather-react-app1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
